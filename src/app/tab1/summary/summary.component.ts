844a102fa89e36a4d607612dafac16e205d9cbcd
sunellopez <sunellopez@gmail.com>
2025-07-28 18:15:12 -0600

    v0.0.4 1.-Se removieron imports inecesarios.

--- src/app/tab1/summary/summary.component.ts
+++ src/app/tab1/summary/summary.component.ts
@@ -1,14 +1,14 @@
 import { CurrencyPipe } from '@angular/common';
-import { Component, input, OnInit, signal } from '@angular/core';
+import { Component, input, OnInit } from '@angular/core';
 import { Summary } from '@interfaces';
-import { IonContent, IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonList, IonItem, IonLabel, IonNote, IonSkeletonText } from "@ionic/angular/standalone";
+import { IonCard, IonCardHeader, IonCardTitle, IonCardSubtitle, IonCardContent, IonList, IonItem, IonLabel, IonNote, IonSkeletonText } from "@ionic/angular/standalone";
 
 @Component({
   selector: 'app-summary',
   templateUrl: './summary.component.html',
   styleUrls: ['./summary.component.scss'],
   standalone: true,
-  imports: [CurrencyPipe, IonSkeletonText, IonNote, IonLabel, IonItem, IonList, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard, IonContent]
+  imports: [CurrencyPipe, IonSkeletonText, IonNote, IonLabel, IonItem, IonList, IonCardContent, IonCardSubtitle, IonCardTitle, IonCardHeader, IonCard]
 })
 export class SummaryComponent  implements OnInit {
   summary = input<Summary>({
